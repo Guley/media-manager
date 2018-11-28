10e08ad47afa085d3c84179bcbc4be2095de2dea
Gulshan <gul2787@gmail.com>
2018-11-28 13:23:06 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://www.media.dev';
+$config['base_url'] = 'https://github.com/Guley/Codeigniter-media-manager/';
 
 /*
 |--------------------------------------------------------------------------
