972da1bab4342eef8a8980844634c14cea27fcca
Gulshan <gul2787@gmail.com>
2018-11-28 13:26:16 +0530

    Create Deploy.php

---
+++ application/controllers/Deploy.php
@@ -0,0 +1,6 @@
+array(
+    'codeigniter-media-manager' => array(
+        'master' => array('base_path' => '/path/PHP-Git-Deploy_1/'),
+        'develop' => array('base_path' => '/path/PHP-Git-Deploy_2/')
+    )
+);
