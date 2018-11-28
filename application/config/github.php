32492fe428b86d33ceaf13cd85ec9e3734df25c3
Gulshan <gul2787@gmail.com>
2018-11-28 13:22:24 +0530

    Create github.php

---
+++ application/config/github.php
@@ -0,0 +1,5 @@
+$config['github'] = array(
+    'Codeigniter-media-manager' => array(
+        'branch_name' => array('base_path' => 'https://github.com/Guley/Codeigniter-media-manager/')
+    )
+);
